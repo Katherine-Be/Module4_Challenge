c0f034f1325663109d52cc06427c454fa44a0864
Katherine Bensley <katherine.bensley.93@gmail>
2024-03-07 20:59:09 -0500

    basic html

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//noting all variables that may change or have something happen to them
+var scoreboard = document.getElementById("scoreboard");
+var questionBox = document.getElementById("quiz'");
+var submitButton = document.getElementById("submit");
+var grade = document.getElementById("grade");
+var timer = document.getElementById("timer");
+var score = document.getElementById("playerScore");
