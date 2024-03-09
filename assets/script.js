4d370324c13714459ab4ad365fc32055a006a186
Katherine Bensley <katherine.bensley.93@gmail>
2024-03-09 13:42:47 -0500

    buttons and basic functions added

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,14 @@ var submitButton = document.getElementById("submit");
 var grade = document.getElementById("grade");
 var timer = document.getElementById("timer");
 var score = document.getElementById("playerScore");
+
+
+function submitAnswer () {//basic example for the function called when user's answer is submitted without question variables or adding to the score.
+    const grade = document.getElementById("grade"); //changed variable "grade" from a var to a const within the function
+    if (response = "A") {
+        grade.innerHTML = "Correct!";//resplaces "correct or incorrect" with the result of the question answer
+    }
+    else {
+        grade.innerHTML = "Incorrect!";
+    }
+}
